a35140f81f6e56c6509334d3f010abdbf748fc8b
syber <syber@crazypanda.ru>
2019-10-22 23:53:29 +0300

    protocol-http: misc

--- plus/Protocol-HTTP/src/panda/protocol/http/Body.h
+++ plus/Protocol-HTTP/src/panda/protocol/http/Body.h
@@ -13,7 +13,7 @@ struct Body {
     Body () {}
     Body (const string& body) { parts.emplace_back(body); }
 
-    Body (const std::initializer_list<string>& l) { for (auto& s : l) parts.emplace_back(s); }
+    Body (const std::initializer_list<string>& l) { for (auto& s : l) parts.push_back(s); }
 
     Body (const Body&) = default;
     Body (Body&& oth)  = default;
