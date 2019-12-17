85a24212782a87d83208472e1fb2e5ad216201ab
Ivan Baidakou <the.dmol@yandex.by>
2019-12-17 20:20:24 +0300

    [SRV-1606] minor fixes

--- plus/Protocol-HTTP/src/panda/protocol/http/Error.cc
+++ plus/Protocol-HTTP/src/panda/protocol/http/Error.cc
@@ -8,14 +8,17 @@ const char* ErrorCategory::name () const throw() { return "protocol-http"; }
 
 std::string ErrorCategory::message (int condition) const throw() {
     switch ((errc)condition) {
-        case errc::semantic_error          : return "http parsing semantic error";
-        case errc::lexical_error           : return "http parsing lexical error";
-        case errc::multiple_content_length : return "multiple content-length header in message is not allowed";
-        case errc::headers_too_large       : return "headers is bigger than max_headers_size";
-        case errc::body_too_large          : return "body is bigger than max_body_size";
-        case errc::unexpected_body         : return "body is prohibited";
-        case errc::unexpected_eof          : return "http parsing error: unexpected EOF";
-        case errc::unexpected_continue     : return "response code 100-continue was not expected";
+        case errc::semantic_error                : return "http parsing semantic error";
+        case errc::lexical_error                 : return "http parsing lexical error";
+        case errc::multiple_content_length       : return "multiple content-length header in message is not allowed";
+        case errc::headers_too_large             : return "headers is bigger than max_headers_size";
+        case errc::body_too_large                : return "body is bigger than max_body_size";
+        case errc::uncompression_failure         : return "payload cannot be uncompressed";
+        case errc::unexpected_body               : return "body is prohibited";
+        case errc::unexpected_eof                : return "http parsing error: unexpected EOF";
+        case errc::unexpected_continue           : return "response code 100-continue was not expected";
+        case errc::unsupported_compression       : return "compression method is not supported";
+        case errc::unsupported_transfer_encoding : return "transfer encodinng is not supported";
     }
     return {};
 }
